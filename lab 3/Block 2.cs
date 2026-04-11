401364b1638ec456ef3f378a2b7397db83850323
OlehUstenko <olehustenko7@gmail.com>
2026-04-11 12:08:57 +0300

    створив дайл для блоку 2

---
+++ lab 3/Block 2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_3
+{
+    internal class Block_2
+    {
+
+    }
+}
