288ea18bb24c5f8547ef8ff257bb6bd065f88898
OlehUstenko <olehustenko7@gmail.com>
2026-04-12 13:11:57 +0300

    переробив через лісти

--- lab 3/Block 1 List .cs
+++ lab 3/Block 1 List .cs
@@ -12,55 +12,48 @@ namespace lab_3
         public static void Block1ListStart()
         {
             Console.OutputEncoding = Encoding.UTF8;
-            int[] arr = FillingMethodList();
-            PrintArrayList(arr, "Масив до обробки: ");
-            RemoveElementsList(ref arr);
-            PrintArrayList(arr, "Масив після видалення:");
+            List<int> list = FillingMethodList();
+            PrintList(list, "Масив до обробки: ");
+            RemoveElementsList(list);
+            PrintList(list, "Масив після видалення:");
         }
-        public static int[] FillingMethodList()
+        public static List<int> FillingMethodList()
         {
             Console.Write("Яким чином  бажаєте заповнити масив? \n" +
                "1. випадковим чином\n" +
                "2. вручну через пробіл\n" +
                "Ваш вибір: ");
             int choise = int.Parse(Console.ReadLine());
-            Console.Write("Введіть розмір масиву: ");
-            int size = int.Parse(Console.ReadLine());
-            int[] array = new int[size];
+            //Console.Write("Введіть розмір масиву: ");
+            //int size = int.Parse(Console.ReadLine());
+            List<int> list = new List<int>();
 
             switch (choise)
             {
                 case 1:
-                    array = RandomArrayList(size);
+                    list = RandomArrayList();
                     break;
                 case 2:
-                    array = HandSpaceList(size);
+                    list = HandSpaceList();
                     break;
 
                 default:
                     Console.WriteLine("Некоректний вибір способу заповнення. Обрано рандом.");
-                    array = RandomArrayList(size);
+                    list = RandomArrayList();
                     break;
             }
-            return array;
+            return list;
         }
-        public static int[] HandSpaceList(int size)
+        public static List<int> HandSpaceList()
         {
             Console.WriteLine("Введіть значення масиву через пробіл:");
-            string input = Console.ReadLine();
-
-            string[] parts = input.Split(' ', '\t');
-
-            int[] array = new int[size];
-
-            for (int i = 0; i < size; i++)
-            {
-                array[i] = int.Parse(parts[i]);
-            }
-            Console.WriteLine("\n");
-            return array;
+            List<int> list = Console.ReadLine()
+                .Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)//розбиваємо
+                .Select(int.Parse)//парсимо в інт
+                .ToList();//і перетворюємо на ліст 
+            return list;
         }
-        public static int[] RandomArrayList(int size)
+        public static List<int> RandomArrayList()
         {
             Console.Write("Введіть мінімальне значення рандому: ");
             int min = int.Parse(Console.ReadLine());
@@ -68,26 +61,26 @@ namespace lab_3
             Console.Write("Введіть максимальне значення рандому: ");
             int max = int.Parse(Console.ReadLine());
 
-            int[] array = new int[size];
+            Console.Write("Скільки елементів у створити: ");
+            int size = int.Parse(Console.ReadLine());
+
+            List<int> list = new List<int>();
             Random random = new Random();
 
             for (int i = 0; i < size; i++)
             {
-                array[i] = random.Next(min, max);
+                list.Add(random.Next(min, max)); //додаємо в кінець 
             }
             Console.WriteLine("\n");
-            return array;
+            return list;
         }
-        public static void PrintArrayList(int[] arr, string message)
+        public static void PrintList(List<int> list , string message)
         {
             Console.WriteLine(message);
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.Write(arr[i] + " ");
-            }
-            Console.WriteLine("\nДовжина масиву: " + arr.Length);
+            Console.WriteLine(string.Join(" ",list));
+            Console.WriteLine("\nДовжина масиву: " + list.Count);
         }
-        public static void RemoveElementsList(ref int[] arr)
+        public static void RemoveElementsList(List<int> list)
         {
             Console.WriteLine("Введіть скільки елементів треба знищити: ");
             int T = int.Parse(Console.ReadLine());
@@ -98,15 +91,15 @@ namespace lab_3
                 Console.WriteLine("Помилка: Індекс K не може бути від'ємним. Операцію скасовано.");
                 return;
             }
-            if (K >= arr.Length)
+            if (K >= list.Count)
             {
-                Console.WriteLine($"Помилка: Індекс {K + 1} поза межами масиву (довжина {arr.Length}).");
+                Console.WriteLine($"Помилка: Індекс {K + 1} поза межами масиву (довжина {list.Count}).");
                 return;
             }
             int count = T;
-            if (K + T > arr.Length)
+            if (K + T > list.Count)
             {
-                count = arr.Length - K;
+                count = list.Count - K;
                 Console.WriteLine($"Попередження: Буде видалено {count} елементів (все, що було доступно).");
             }
 
@@ -115,11 +108,8 @@ namespace lab_3
                 Console.WriteLine("Кількість елементів для видалення має бути більшою за 0.");
                 return;
             }
-            for (int i = K; i < arr.Length - count; i++)
-            {
-                arr[i] = arr[i + count];
-            }
-            Array.Resize(ref arr, arr.Length - count);
+
+            list.RemoveRange(K, count);
 
             Console.WriteLine("Елементи успішно видалено.");
         }
--- lab 3/Block 2 List .cs
+++ lab 3/Block 2 List .cs
@@ -3,57 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace lab_3
 {
     internal class Block_2_lList
     {
-        public static void Block2Start()
+        public static void Block2ListStart()
         {
             Console.OutputEncoding = Encoding.UTF8;
             Console.WriteLine("Оберіть дію" +
                 "\n1.Заповнити масив вручну" +
                 "\n2. Заповнити масив рандом");
             string choise = Console.ReadLine();
-            int[][] array = null;
+            List<List<int>> matrix = null;
+            
             switch (choise)
             {
                 case "1":
-                    array = ManualFilling();
+                    matrix = ManualFilling();
                     break;
                 case "2":
-                    array = RandomFilling();
+                    matrix = RandomFilling();
                     break;
                 default:
                     Console.WriteLine("Error");
                     break;
             }
-            PrintArray(array);
-            RemoveRows(ref array);
-            PrintArray(array);
+            PrintArray(matrix);
+            RemoveRows(ref matrix);
+            PrintArray(matrix);
         }
-        public static int[][] ManualFilling()
+        public static List<List<int>> ManualFilling()
         {
             Console.WriteLine("Введіть клькіть рядків: ");//прибрати повторюваний код
             int rows = int.Parse(Console.ReadLine());
 
-            int[][] array = new int[rows][];
+            List<List<int>> matrix = new List<List<int>>();
 
             for (int i = 0; i < rows; i++)
             {
                 Console.WriteLine($"Введіть рядок {i + 1} через пробіли : ");
-                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
-                array[i] = Array.ConvertAll(input, int.Parse);
+                List<int> row = Console.ReadLine()
+                     .Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                     .Select(int.Parse)
+                     .ToList();
+                matrix.Add(row);
             }
-            return array;
+            return matrix;
         }
-        public static int[][] RandomFilling()
+        public static List<List<int>> RandomFilling()
         {
             Random random = new Random();
             Console.WriteLine("Введіть кількість рядків: ");//прибрати повторюваний код
             int rows = int.Parse(Console.ReadLine());
 
-            int[][] array = new int[rows][];
+            List<List<int>> matrix = new List<List<int>>();
 
             Console.Write("Введіть мінімальне значення рандому: ");
             int min = int.Parse(Console.ReadLine());
@@ -64,29 +69,27 @@ namespace lab_3
             for (int i = 0; i < rows; i++)
             {
                 Console.Write($"Введіть кількість елементів рядка {i + 1} : ");
-                int elemets = int.Parse(Console.ReadLine());
-                array[i] = new int[elemets];
-                for (int j = 0; j < elemets; j++)
+                int elements = int.Parse(Console.ReadLine());
+                List<int> row = new List<int>();
+                for (int j = 0; j < elements; j++)
                 {
-                    array[i][j] = random.Next(min, max);
+                    row.Add(random.Next(min, max));
                 }
+                matrix.Add(row);
             }
-            return array;
+            return matrix;
         }
-        public static void PrintArray(int[][] array)
+        public static void PrintArray(List<List<int>> matrix)
         {
-            int rows = array.Length;
-            Console.WriteLine("Поточний масив");
-            for (int i = 0; i < rows; i++)
+            Console.WriteLine("\nПоточний зубчастий список:");
+            for (int i = 0; i < matrix.Count; i++)
             {
-                foreach (int el in array[i])
-                {
-                    Console.Write(el + " ");
-                }
-                Console.WriteLine();
+                Console.Write($"Рядок {i} (Count: {matrix[i].Count}):\t");
+                Console.WriteLine(string.Join(" ", matrix[i]));
             }
+            Console.WriteLine();
         }
-        public static void RemoveRows(ref int[][] array)
+        public static void RemoveRows(List<List<int>> matrix)
         {
             Console.Write("Введіть порядковий номер рядка з якого починати : ");
             int k1 = int.Parse(Console.ReadLine());
@@ -99,18 +102,16 @@ namespace lab_3
                 (k1, k2) = (k2, k1);
             }
 
-            if (k2 > array.Length || k1 < 1)
+            if (k2 > matrix.Count || k1 < 1)
             {
                 Console.WriteLine("ви вийшли за межі");
                 return;
             }
 
-            int count = k2 - k1 + 1;
-            for (int i = k1 - 1; i < array.Length - count; i++)
-            {
-                array[i] = array[i + count];
-            }
-            Array.Resize(ref array, array.Length - count);
+            int index = k1 - 1; 
+            int count = k2 - k1 + 1; 
+
+            matrix.RemoveRange(index, count);
 
             Console.WriteLine($"Успішно видалено {count} рядків.");
 
